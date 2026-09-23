94d0c7fc18db3f807d21f5f0e3116e1ed886308e
Rktutorial <sgronak444@gmail.com>
2026-09-23 20:44:29 +0530

    Add files via upload

---
+++ sketch_sep23ablink.ino
@@ -0,0 +1,19 @@
+const int led=2;
+
+void setup() {
+
+  
+  pinMode(2,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(2,1);
+  delay(1000);
+  digitalWrite(2,0);
+  delay(1000);
+
+  
+
+}
